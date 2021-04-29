28cd34d64f90dd7501f794140447d8b512b4d11a
arthur <arthur@kaise.com.tw>
2021-04-29 13:43:44 +0800

    v0.2.1

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# pxt-KSRobot-Sensor
+# KSRobot Sensor for micro-bit
+
+## User Guide 
+
+## Code Example 
+
+## License
+
+MIT
+
+The code of ultrasonic was coded by Microsoft
+https://github.com/Microsoft/pxt-sonar
+
+The code of DHT11 was coded by MonadnockSystems
+https://github.com/alankrantas/pxt-dht11_dht22
+
+
+The code of IIC 1602 Panel was coded by lioujj
+https://github.com/lioujj/pxt-lcd1602
+
+## Supported targets
+
+* for PXT/microbit
+(The metadata above is needed for package search.)
---
+++ main.ts
@@ -0,0 +1,201 @@
+//% color=#ff4b4b icon="\uf2db" block="KSRobot_Sensor"
+namespace KSRobot_Sensor {
+
+
+    export enum DHT_type {
+        //% blockId="DHT11" block="DHT11"
+        DHT11,
+        //% blockId="DHT22" block="DHT22"
+        DHT22,
+    }
+
+    export enum DHT_State {
+        //% blockId="Celsius" block="Celsius"
+        Celsius,
+        //% blockId="Fahrenheit" block="Fahrenheit"
+        Fahrenheit,
+        //% blockId="Humidity" block="Humidity"
+        Humidity,
+    }
+
+    export enum SOIL_State {
+        //% blockId="Celsius" block="Celsius"
+        Celsius,
+        //% blockId="Humidity" block="Humidity"
+        Humidity,
+    }
+
+
+    //% blockId="KSRobot_dht11" block="DHT set %dht_type pin %dataPin|get %dht_state"
+    export function dht_readdata(dht_type: DHT_type, dataPin: DigitalPin, dht_state: DHT_State): number {
+        let dataArray: boolean[] = []
+        let resultArray: number[] = []
+        let checksum: number = 0
+        let checksumTmp: number = 0
+        let _temperature: number = -999.0
+        let _humidity: number = -999.0
+        let _readSuccessful: boolean = false
+
+        for (let index = 0; index < 40; index++) dataArray.push(false)
+        for (let index = 0; index < 5; index++) resultArray.push(0)
+
+        pins.digitalWritePin(dataPin, 0)
+        basic.pause(18)
+        pins.setPull(dataPin, PinPullMode.PullUp);
+        pins.digitalWritePin(dataPin, 1)
+        control.waitMicros(40)
+        pins.digitalReadPin(dataPin)
+        
+        if (pins.digitalReadPin(dataPin) != 1) {
+            while (pins.digitalReadPin(dataPin) == 1);
+            while (pins.digitalReadPin(dataPin) == 0);
+            while (pins.digitalReadPin(dataPin) == 1);
+            //read data (5 bytes)
+            for (let index = 0; index < 40; index++) {
+                while (pins.digitalReadPin(dataPin) == 1);
+                while (pins.digitalReadPin(dataPin) == 0);
+                control.waitMicros(28)
+                //if sensor still pull up data pin after 28 us it means 1, otherwise 0
+                if (pins.digitalReadPin(dataPin) == 1) dataArray[index] = true
+            }
+
+            //convert byte number array to integer
+            for (let index = 0; index < 5; index++)
+                for (let index2 = 0; index2 < 8; index2++)
+                    if (dataArray[8 * index + index2]) resultArray[index] += 2 ** (7 - index2)
+
+            //verify checksum
+            checksumTmp = resultArray[0] + resultArray[1] + resultArray[2] + resultArray[3]
+            checksum = resultArray[4]
+            if (checksumTmp >= 512) checksumTmp -= 512
+            if (checksumTmp >= 256) checksumTmp -= 256
+            if (checksum == checksumTmp) _readSuccessful = true
+
+            //read data if checksum ok
+            if (_readSuccessful) {
+                if (dht_type == DHT_type.DHT11) {
+                    //DHT11
+                    _humidity = resultArray[0] + resultArray[1] / 100
+                    _temperature = resultArray[2] + resultArray[3] / 100
+                } else {
+                    //DHT22
+                    let temp_sign: number = 1
+                    if (resultArray[2] >= 128) {
+                        resultArray[2] -= 128
+                        temp_sign = -1
+                    }
+                    _humidity = (resultArray[0] * 256 + resultArray[1]) / 10
+                    _temperature = (resultArray[2] * 256 + resultArray[3]) / 10 * temp_sign
+                }
+                switch (dht_state) {
+                    case DHT_State.Celsius:
+
+                        return _temperature;
+                        break;
+
+                    case DHT_State.Fahrenheit:
+
+                        return _temperature = _temperature * 9 / 5 + 32;
+                        break;
+
+                    case DHT_State.Humidity:
+
+                        return _humidity;
+                        break;
+
+                }
+            }
+
+        }
+
+        return -99;
+
+    }
+
+
+    //% blockId="KSRobot_soil" block="Soil pin %dataPin|get %soil_state"
+    export function soil_readdata(dataPin: DigitalPin, soil_state: SOIL_State): number {
+        let dataArray: boolean[] = []
+        let resultArray: number[] = []
+        let checksum: number = 0
+        let checksumTmp: number = 0
+        let _temperature: number = -999.0
+        let _humidity: number = -999.0
+        let _readSuccessful: boolean = false
+
+        for (let index = 0; index < 40; index++) dataArray.push(false)
+        for (let index = 0; index < 5; index++) resultArray.push(0)
+
+        pins.digitalWritePin(dataPin, 0)
+        basic.pause(18)
+        pins.setPull(dataPin, PinPullMode.PullUp);
+        pins.digitalWritePin(dataPin, 1)
+        control.waitMicros(40)
+        pins.digitalReadPin(dataPin)
+       
+        if (pins.digitalReadPin(dataPin) != 1) {
+            while (pins.digitalReadPin(dataPin) == 1);
+            while (pins.digitalReadPin(dataPin) == 0);
+            while (pins.digitalReadPin(dataPin) == 1);
+            //read data (5 bytes)
+            for (let index = 0; index < 40; index++) {
+                while (pins.digitalReadPin(dataPin) == 1);
+                while (pins.digitalReadPin(dataPin) == 0);
+                control.waitMicros(28)
+                //if sensor still pull up data pin after 28 us it means 1, otherwise 0
+                if (pins.digitalReadPin(dataPin) == 1) dataArray[index] = true
+            }
+
+            //convert byte number array to integer
+            for (let index = 0; index < 5; index++)
+                for (let index2 = 0; index2 < 8; index2++)
+                    if (dataArray[8 * index + index2]) resultArray[index] += 2 ** (7 - index2)
+
+            //verify checksum
+            checksumTmp = resultArray[0] + resultArray[1] + resultArray[2] + resultArray[3]
+            checksum = resultArray[4]
+            if (checksumTmp >= 512) checksumTmp -= 512
+            if (checksumTmp >= 256) checksumTmp -= 256
+            if (checksum == checksumTmp) _readSuccessful = true
+
+            //read data if checksum ok
+            if (_readSuccessful) {
+
+                _humidity = resultArray[0] + resultArray[1] / 100
+                _temperature = resultArray[2] + resultArray[3] / 100
+
+                switch (soil_state) {
+                    case SOIL_State.Celsius:
+
+                        return _temperature;
+                        break;
+
+                    case SOIL_State.Humidity:
+
+                        return _humidity;
+                        break;
+
+                }
+            }
+
+        }
+
+        return -99;
+
+    }
+
+    //% blockId="KSRobot_temt6000" block="TEMT6000(Lux) set pin %dataPin"
+    export function temt6000(dataPin: AnalogPin): number {
+        let temp = pins.analogReadPin(dataPin)
+        return (Math.round(temp * 4 / 1024 / 10000 * 1000000 * 4))
+    }
+
+    //% blockId="KSRobot_wind_sensor1" block="Wind Sensor(m/s) set pin %dataPin"
+    export function wind_sensor1(dataPin: AnalogPin): number {
+        let temp = pins.analogReadPin(dataPin)
+        return (temp * 4 / 1024 * 26)
+    }
+
+
+
+}
